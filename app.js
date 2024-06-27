a1ecf99c15922fc3807552ab6785d40622966958
Iván <ivanbailetti@hotmail.com>
2024-06-27 16:47:23 -0300

    [Add] Add new step Info containing the information about the deployment

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
 
+const {version} = require('./package.json')
 const PORT = process.env.PORT || 5000
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send(version)
+})
 
 app.use(express.static('dist'))
 
